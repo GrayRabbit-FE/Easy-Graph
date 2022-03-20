e2912f8bd655ce80e1582907b73005e02f7e6bf9
飞鸟 <965555169@qq.com>
2022-03-20 19:49:58 +0800

    feat: update Context

--- src/components/EGContext/EGContext.tsx
+++ src/components/EGContext/EGContext.tsx
@@ -1,22 +1,26 @@
 import React, { useEffect } from 'react';
 import { EGContextProps } from '../../typings/Components';
 import { useDrag } from './useDrag';
+import { useScale } from './useScale';
 // TODO:找不到模块“./EGContext.less”或其相应的类型声明。
 // 明明已经添加了less的模块声明
 import s from './EGContext.less';
 
 const EGContext: React.FC<EGContextProps> = ({ nodes, edges, options, children }) => {
+  // TODO: 加入节流
   const { trans, handleMouseDown, handleMouseMove, setIsMoving } = useDrag();
+  const { scale, handleScroll } = useScale();
 
   return (
     <div
       className={s.contextBox}
       style={{
-        transform: `translate(${trans.x}px, ${trans.y}px)`
+        transform: `translate(${trans.x}px,${trans.y}px) scale(${scale})`
       }}
       onMouseDown={handleMouseDown}
       onMouseMove={handleMouseMove}
       onMouseUp={() => setIsMoving(false)}
+      onWheel={handleScroll}
     >
       {children}
     </div>
